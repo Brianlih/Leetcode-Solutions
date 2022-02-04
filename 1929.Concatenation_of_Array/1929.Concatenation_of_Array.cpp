3e596735039e141a8104e63f0f68485551b0a4d3
Brianlih <brianlih410@gmail.com>
2022-02-05 00:12:22 +0800

    finish leetcode#1929

---
+++ 1929.Concatenation_of_Array/1929.Concatenation_of_Array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int length = nums.size();
+        for (int i = 0; i < length; i++) {
+            nums.insert(nums.end(), nums.begin(), nums.end() - 1);
+            for (int j = 0; j < nums.size(); j++) {
+                cout << nums[j];
+            }
+            cout << endl;
+        }
+
+        return nums;
+    }
+};
+
+int main()
+{
+    int a[6] = {1,2,3,4,5,6};
+    vector<int> nums;
+    for (int i = 0; i < sizeof(a)/4; i++) {
+        nums.push_back(a[i]);
+    }
+    Solution sol;
+    sol.getConcatenation(nums);
+    int len = sizeof(nums)/4;
+    for (int i = 0; i < len; i++) {
+        cout << endl << nums[i] << endl;
+    }
+
+    return 0;
+
+}
