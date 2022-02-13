8a166c5853da270c0dc364eccc62808d01752924
Brianlih <brianlih410@gmail.com>
2022-02-13 19:01:47 +0800

    finish leetcode#1528

---
+++ Easy/1528.Shuffle_String/1528.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string answer = s;
+        for (int i = 0; i < indices.size(); i++) {
+            answer[indices[i]] = s[i];
+        }
+        
+        return answer;
+    }
+};
