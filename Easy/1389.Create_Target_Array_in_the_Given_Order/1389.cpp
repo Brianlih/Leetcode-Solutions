581f1d692ca8e2c5a64f423bb7f056a93b1a8d68
Brianlih <brianlih410@gmail.com>
2022-02-17 22:40:22 +0800

    finish leetcode#1389

---
+++ Easy/1389.Create_Target_Array_in_the_Given_Order/1389.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> answer;
+        for (int i = 0; i < index.size(); i++) {
+            answer.insert(answer.begin() + index[i], nums[i]);
+        }
+        
+        return answer;
+    }
+};
